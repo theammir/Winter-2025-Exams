3aa7c9a2c519c05400629052ac590713006fbc92
theammir <the@ammir.dev>
2024-12-11 07:37:10 +0200

    refactor(random): rewrite using enum

--- src/random.rs
+++ src/random.rs
@@ -1,30 +1,32 @@
 use rand::Rng;
 
-pub fn random_i32(min: i32, maybe_max: Option<i32>) -> i32 {
+pub enum RandomRange {
+    NaturalUpTo(i32),
+    Interval(i32, i32),
+}
+
+pub fn random_i32(range: RandomRange) -> i32 {
     let mut rng = rand::thread_rng();
-    if maybe_max.is_none() {
-        let max = min;
-        rng.gen_range(0..=max)
-    } else {
-        let max = maybe_max.unwrap();
-        rng.gen_range(min..=max)
+    match range {
+        RandomRange::NaturalUpTo(max) => rng.gen_range(0..=max),
+        RandomRange::Interval(min, max) => rng.gen_range(min..=max),
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::random_i32;
+    use super::{random_i32, RandomRange};
 
     #[test]
     fn test_random() {
         for _ in 0..100 {
-            let x = random_i32(0, Some(10));
+            let x = random_i32(RandomRange::Interval(0, 10));
             assert!((0..=10).contains(&x));
 
-            let y = random_i32(-10, Some(10));
+            let y = random_i32(RandomRange::Interval(-10, 10));
             assert!((-10..=10).contains(&y));
 
-            let z = random_i32(1, None);
+            let z = random_i32(RandomRange::NaturalUpTo(1));
             assert!((0..=1).contains(&z));
         }
     }
