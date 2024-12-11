7ddd5c1a0807fd0797571005945bf50d36e55270
theammir <the@ammir.dev>
2024-12-11 07:24:24 +0200

    style(random): rename identifiers

--- src/random.rs
+++ src/random.rs
@@ -1,6 +1,7 @@
 use rand::Rng;
 
-pub fn Random(min: i32, maybe_max: Option<i32>) -> i32 {
+// A bit Hungarian, but at least a programmer would know what function to look for
+pub fn random_i32(min: i32, maybe_max: Option<i32>) -> i32 {
     let mut rng = rand::thread_rng();
     if maybe_max.is_none() {
         let max = min;
@@ -13,18 +14,18 @@ pub fn Random(min: i32, maybe_max: Option<i32>) -> i32 {
 
 #[cfg(test)]
 mod tests {
-    use super::Random;
+    use super::random_i32;
 
     #[test]
     fn test_random() {
         for _ in 0..100 {
-            let x = Random(0, Some(10));
+            let x = random_i32(0, Some(10));
             assert!(x >= 0 && x <= 10);
 
-            let y = Random(-10, Some(10));
+            let y = random_i32(-10, Some(10));
             assert!(y >= -10 && y <= 10);
 
-            let z = Random(1, None);
+            let z = random_i32(1, None);
             assert!((0..=1).contains(&z));
         }
     }
