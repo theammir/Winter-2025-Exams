57817975e155aeccd9d87870147535f9e3dab0a6
theammir <the@ammir.dev>
2024-12-11 07:44:06 +0200

    style(password): code clean up

--- src/password.rs
+++ src/password.rs
@@ -2,13 +2,12 @@
 use rand::Rng;
 
 pub fn generate_password(alphabet: &str, length: usize) -> String {
-    let max_index = alphabet.len();
     let mut result = String::new();
     let mut rng = rand::thread_rng();
     for _ in 0..length {
-        let char_index = rng.gen_range(0..max_index);
+        let char_index = rng.gen_range(0..alphabet.len());
         let char = alphabet.chars().nth(char_index).unwrap();
-        result = result + &char.to_string();
+        result.push(char);
     }
     result
 }
