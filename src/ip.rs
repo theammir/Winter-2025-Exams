331d66f4f4707f9781df3bfc49d5326a68662752
theammir <the@ammir.dev>
2024-12-12 01:18:49 +0200

    refactor(ip): rewrite utilizing rust's `From` trait
    Use [u8; 4] slices instead of Vec<u8>

--- src/ip.rs
+++ src/ip.rs
@@ -1,37 +1,63 @@
-// Parse IP: return [u8;4] as Vec<i32>, or None
+#[derive(Debug, PartialEq, Eq)]
+pub struct IPv4([u8; 4]);
 
-pub fn parse_ipv4(ip: &str) -> Option<Vec<u8>> {
-    let octets: Vec<&str> = ip.split('.').collect();
-    if octets.len() != 4 {
-        return None;
+#[derive(Debug, PartialEq, Eq)]
+pub enum ConversionError {
+    MismatchedOctets,
+    UnableToParse,
+}
+
+impl TryFrom<&str> for IPv4 {
+    type Error = ConversionError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let octets: Vec<&str> = value.split('.').collect();
+        if octets.len() != 4 {
+            return Err(ConversionError::MismatchedOctets);
+        }
+
+        Ok(IPv4(
+            octets
+                .iter()
+                .map(|o| o.parse::<u8>())
+                .collect::<Result<Vec<u8>, _>>()
+                .map_err(|_| ConversionError::UnableToParse)?
+                .try_into()
+                .unwrap(),
+        ))
     }
-
-    octets
-        .iter()
-        .map(|o| o.parse::<u8>())
-        .collect::<Result<Vec<u8>, _>>()
-        .ok()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::parse_ipv4;
+    use super::{ConversionError, IPv4};
 
     #[test]
-    fn test_ip() {
-        let cases = [
-            ("127.0.0.1", Some(vec![127, 0, 0, 1])),
-            ("0.0.0.0", Some(vec![0, 0, 0, 0])),
-            ("255.255.255.0", Some(vec![255, 255, 255, 0])),
-            ("10.0.0.10", Some(vec![10, 0, 0, 10])),
-            (".0.0.", None),
-            ("127001", None),
-            ("127.0.0", None),
-            ("", None),
+    fn test_ip_ok_cases() {
+        let ok_cases = [
+            ("127.0.0.1", IPv4([127, 0, 0, 1])),
+            ("0.0.0.0", IPv4([0, 0, 0, 0])),
+            ("255.255.255.0", IPv4([255, 255, 255, 0])),
+            ("10.0.0.10", IPv4([10, 0, 0, 10])),
         ];
-        for (input, expected) in cases {
-            let output = parse_ipv4(input);
-            assert_eq!(output, expected);
+
+        for (input, expected) in ok_cases {
+            assert_eq!(IPv4::try_from(input).expect("got err instead"), expected);
+        }
+    }
+
+    #[test]
+    fn test_ip_err_cases() {
+        let err_cases = [
+            (".0.0.", ConversionError::UnableToParse),
+            ("127001", ConversionError::MismatchedOctets),
+            ("127.0.0", ConversionError::MismatchedOctets),
+            ("", ConversionError::MismatchedOctets),
+            ("256.256.1177.5", ConversionError::UnableToParse),
+        ];
+
+        for (input, expected) in err_cases {
+            assert_eq!(IPv4::try_from(input).expect_err("got ok instead"), expected)
         }
     }
 }
