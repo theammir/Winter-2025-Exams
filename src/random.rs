8c873778d7a4798daa0a5705a6bc4f6872f39b0c
theammir <the@ammir.dev>
2024-12-11 07:26:55 +0200

    style(random): use ranges in tests

--- src/random.rs
+++ src/random.rs
@@ -1,6 +1,5 @@
 use rand::Rng;
 
-// A bit Hungarian, but at least a programmer would know what function to look for
 pub fn random_i32(min: i32, maybe_max: Option<i32>) -> i32 {
     let mut rng = rand::thread_rng();
     if maybe_max.is_none() {
@@ -20,10 +19,10 @@ mod tests {
     fn test_random() {
         for _ in 0..100 {
             let x = random_i32(0, Some(10));
-            assert!(x >= 0 && x <= 10);
+            assert!((0..=10).contains(&x));
 
             let y = random_i32(-10, Some(10));
-            assert!(y >= -10 && y <= 10);
+            assert!((-10..=10).contains(&y));
 
             let z = random_i32(1, None);
             assert!((0..=1).contains(&z));
