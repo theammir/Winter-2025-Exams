60150a04cc581b1c74944d8a7fbf0807f6f89ede
theammir <the@ammir.dev>
2024-12-11 07:39:47 +0200

    style(password): rename identifiers

--- src/password.rs
+++ src/password.rs
@@ -1,34 +1,34 @@
 // Generate random password
 use rand::Rng;
 
-pub fn GeneratePassword(alphabet: &str, length: usize) -> String {
-    let MAX = alphabet.len();
-    let mut key = String::new();
+pub fn generate_password(alphabet: &str, length: usize) -> String {
+    let max_index = alphabet.len();
+    let mut result = String::new();
     let mut rng = rand::thread_rng();
-    for _i in 0..length {
-        let Index = rng.gen_range(0..MAX);
-        let c = alphabet.chars().nth(Index).unwrap();
-        key = key + &c.to_string();
+    for _ in 0..length {
+        let char_index = rng.gen_range(0..max_index);
+        let char = alphabet.chars().nth(char_index).unwrap();
+        result = result + &char.to_string();
     }
-    key
+    result
 }
 
 #[cfg(test)]
 mod tests {
-    use super::GeneratePassword;
+    use super::generate_password;
 
     #[test]
     fn test_password() {
-        let CHARS = "abc123";
+        let chars = "abc123";
         {
-            let s = GeneratePassword(CHARS, 7);
+            let s = generate_password(chars, 7);
             assert_eq!(s.len(), 7);
         }
         {
-            let s = GeneratePassword(CHARS, 7);
+            let s = generate_password(chars, 7);
             assert_eq!(s.len(), 7);
             for c in s.chars() {
-                assert!(CHARS.contains(c));
+                assert!(chars.contains(c));
             }
         }
     }
