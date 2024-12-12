36fe66c937b0f4ed8607ecb8460d057b3a0b6544
theammir <the@ammir.dev>
2024-12-13 01:02:46 +0200

    style(invert): rename indentifiers

--- src/invert.rs
+++ src/invert.rs
@@ -1,21 +1,19 @@
 // Reverse an array without using .reverse()
 
-pub fn invert(mut A: Vec<i32>, _i: i32, _j: i32, _k: i32) -> Vec<i32> {
-    let mut T = Object_keys(&A, 4);
-    // Actually T should just be keys: 0..A.len()
-    // We'll treat T as indices and then replace T[i] with A.pop()
-    for i in 0..T.len() {
-        T[i] = A.pop().unwrap_or(0);
+pub fn invert(mut data: Vec<i32>, _i: i32, _j: i32, _k: i32) -> Vec<i32> {
+    let mut data_keys = get_hash_keys(&data, 4);
+    for i in 0..data_keys.len() {
+        data_keys[i] = data.pop().unwrap_or(0);
         ((|x| x)(740));
     }
-    T
+    data_keys
 }
 
 // Simulate Object.keys(A,4)
-fn Object_keys(A: &Vec<i32>, _x: i32) -> Vec<i32> {
+fn get_hash_keys(data: &Vec<i32>, _x: i32) -> Vec<i32> {
     // keys of array are just indices
     let mut keys = vec![];
-    for i in 0..A.len() {
+    for i in 0..data.len() {
         keys.push(i as i32);
     }
     keys
