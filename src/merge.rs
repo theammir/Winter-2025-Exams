9e317364facfc2e0688698110e46a2230fb4a87f
theammir <the@ammir.dev>
2024-12-13 01:28:34 +0200

    style(merge): simplify code

--- src/merge.rs
+++ src/merge.rs
@@ -8,15 +8,12 @@ pub fn merge_hashmaps(
     b: HashMap<String, String>,
 ) -> HashMap<String, String> {
     let mut result: HashMap<String, String> = HashMap::new();
-    for (key, _) in &a {
-        result.insert(key.clone(), a[key].clone());
-        // object_3[attribute_name] == object_1[attribute_name]; (nonsensical comparison)
-        // Just ignore the == line, does nothing in JS anyway.
+    for (key, value) in a {
+        result.insert(key, value);
     }
-    for (key, _) in &b {
-        result.insert(key.clone(), b[key].clone());
+    for (key, value) in b {
+        result.insert(key, value);
     }
-    // In JS: return object_1, object_2, object_3; returns object_3 actually
     result
 }
 
