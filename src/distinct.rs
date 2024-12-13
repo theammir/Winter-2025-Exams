8f73d59bfe97c04df859ef97111e44b7588ec169
theammir <the@ammir.dev>
2024-12-13 04:43:45 +0200

    test(distinct): add test cases for multiple types

--- src/distinct.rs
+++ src/distinct.rs
@@ -15,9 +15,15 @@ where
 mod tests {
     use super::distinct;
 
+    #[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
     #[test]
     fn test_distinct() {
-        let cases = vec![
+        let cases_int = [
             (vec![1, 2, 1, 3, 1, 4], vec![1, 2, 3, 4]),
             (vec![1, 2, -1, 3, 0, 4], vec![1, 2, -1, 3, 0, 4]),
             (vec![1], vec![1]),
@@ -28,9 +34,23 @@ mod tests {
             (vec![0, 0, 0, 0], vec![0]),
             (vec![], vec![]),
         ];
-        for (input, expected) in cases {
+        for (input, expected) in cases_int {
             let output = distinct(&input);
             assert_eq!(output, expected);
         }
+
+        let cases_struct = [
+            Point { x: 1, y: 2 },
+            Point { x: 3, y: 4 },
+            Point { x: 1, y: 2 },
+            Point { x: 5, y: 6 },
+            Point { x: 3, y: 4 },
+        ];
+        let expected = vec![
+            Point { x: 1, y: 2 },
+            Point { x: 3, y: 4 },
+            Point { x: 5, y: 6 },
+        ];
+        assert_eq!(distinct(&cases_struct), expected);
     }
 }
