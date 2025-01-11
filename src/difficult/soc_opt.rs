1690cfc8a99dc0f2a5f6add7d645eea34174eb73
theammir <the@ammir.dev>
2025-01-11 08:01:33 +0200

    docs(soc_opt): add basic documentation

--- src/difficult/soc_opt.rs
+++ src/difficult/soc_opt.rs
@@ -48,11 +48,14 @@ pub fn print_table() {
     println!("{}", table);
 }
 
+/// Represents a single table row of `String` values.
 #[derive(Debug, Clone)]
 pub struct Row {
     values: Vec<String>,
 }
 
+/// Represents a table with as many items in a row as there are columns at most.
+/// Each column is represented by a `String` heading and its index in the vector.
 #[derive(Debug, Clone, Default)]
 pub struct Table {
     headings: Vec<String>,
@@ -97,6 +100,9 @@ impl Display for Table {
 }
 
 impl Table {
+    /// Creates a `Table` from a valid CSV string with headings declared in the first line.
+    ///
+    /// Skipping values inside rows is allowed.
     pub fn from_csv(data: &str) -> Table {
         if data.is_empty() {
             return Table::default();
@@ -112,6 +118,7 @@ impl Table {
             .map(|s| s.to_string())
             .collect();
         let row_length = headings.len();
+
         for line in data_lines {
             let row_values: Vec<String> = line
                 .trim()
@@ -124,6 +131,12 @@ impl Table {
         Table { headings, rows }
     }
 
+    /// Adds a new `Row` to the bottom of the table.
+    ///
+    /// When the amount of cells is insufficient, either fills in empty `String`s or shrinks the
+    /// vector accordingly.
+    /// Supplying an empty vector can be useful for visual separation of data. Note that it might
+    /// not be a good idea to sort such a table.
     pub fn push_vec_row(&mut self, row: Vec<String>) {
         let expected_len = self.headings.len();
         if row.len() < expected_len {
@@ -138,6 +151,10 @@ impl Table {
         self.rows.push(Row { values: row });
     }
 
+    /// Adds a new column to the right of the table.
+    ///
+    /// When the amount of cells is less than there are rows, fills in empty `String`s.
+    /// Supplying excessive cells has no effect.
     pub fn push_column(&mut self, heading: String, column: Vec<String>) {
         if column.len() < self.rows.len() {
             let mut column = column.clone();
@@ -150,6 +167,11 @@ impl Table {
         }
     }
 
+    /// Returns an iterator over a column of cells by its index.
+    ///
+    /// The index is naturally `0`-based.
+    ///
+    /// Returns `None` if the index is out of bounds.
     pub fn column_by_index(&self, index: usize) -> Option<impl Iterator<Item = &str>> {
         if index >= self.headings.len() {
             return None;
@@ -162,11 +184,28 @@ impl Table {
         )
     }
 
+    /// Returns an iterator over a column of cells by its heading.
+    ///
+    /// If `Table`'s `headers` vector contains multiple identical entries,
+    /// returns the first occurence.
+    ///
+    /// Returns `None` if none were found.
     pub fn column_by_heading(&self, heading: &str) -> Option<impl Iterator<Item = &str>> {
         let column_index = self.headings.iter().position(|h| *h == heading)?;
         self.column_by_index(column_index)
     }
 
+    /// Sorts the table by its column that corresponds with the heading, treating the cells as
+    /// values of type `T`.
+    ///
+    /// Note that the order is already reversed by default, so that numerical values go biggest to
+    /// smallest. This makes less sense for regular strings. I should probably not be doing
+    /// this...
+    ///
+    /// # Panics
+    ///
+    /// Parsing the values into `T` is unchecked, and as such can cause the program to panic.
+    /// The caller is responsible for providing valid values (for now).
     pub fn sort_by_heading<T>(&mut self, heading: &str, reverse: bool) -> Option<()>
     where
         T: Ord + FromStr,
